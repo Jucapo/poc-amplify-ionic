26e1a3bb9a1eee8c1e40f16e5507c44a58921390
Juan Camilo Posso <juan.posso@cognits.co>
2025-04-02 17:39:02 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'poc-amplify-ionic',
+  webDir: 'www'
+};
+
+export default config;
