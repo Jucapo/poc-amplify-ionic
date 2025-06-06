c5968d7fd2c07b165977204986851261e07aac97
Juan Camilo Posso <juan.posso@cognits.co>
2025-06-06 15:36:11 -0500

    Test S3 - v2

--- amplify/storage/resource.ts
+++ amplify/storage/resource.ts
@@ -2,4 +2,18 @@ import { defineStorage } from '@aws-amplify/backend';
 
 export const storage = defineStorage({
   name: 'amplifyTeamDrive',
+
+  access: (allow) => ({
+    /* público (guest + auth) */
+    'public/prospects/*': [
+      allow.guest.to(['read', 'write']),
+      allow.authenticated.to(['read', 'write']),
+    ],
+
+    /* protegido (solo usuarios autenticados) */
+    'protected/*': [allow.authenticated.to(['read', 'write'])],
+
+    /* privado (solo el dueño “identity”) */
+    'private/*': [allow.entity('identity').to(['read', 'write', 'delete'])],
+  }),
 });
